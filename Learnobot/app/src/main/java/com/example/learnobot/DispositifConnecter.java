b47b6638683b3a8b53279f3e511e00765a4d0509
Odiouma <odioumacoulibaly46@gmail.com>
2022-12-19 19:58:46 +0100

    connexion bluetooth

--- Learnobot/app/src/main/java/com/example/learnobot/DispositifConnecter.java
+++ Learnobot/app/src/main/java/com/example/learnobot/DispositifConnecter.java
@@ -1,14 +1,96 @@
 package com.example.learnobot;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Set;
 
 public class DispositifConnecter extends AppCompatActivity {
+
+    // Depuración de LOGCAT
+    private static final String TAG = "DispositivosVinculados";
+    // Declaracion de ListView
+    ListView IdLista;
+    // String que se enviara a la actividad principal, mainactivity
+    public static String EXTRA_DEVICE_ADDRESS = "device_address";
+    // Declaracion de campos
+    private BluetoothAdapter mBtAdapter;
+    private ArrayAdapter mPairedDevicesArrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dispositif_connecter);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        //---------------------------------------------------------------------
+        VerificarEstadoBT();
+        mPairedDevicesArrayAdapter = new ArrayAdapter(this, R.layout.dispositif_mconnecter);
+        IdLista = (ListView) findViewById(R.id.idListe);
+        IdLista.setAdapter(mPairedDevicesArrayAdapter);
+        IdLista.setOnItemClickListener(mDeviceClickListener);
+        mBtAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+            Set<BluetoothDevice> pairedDevices = mBtAdapter.getBondedDevices();
+            if (pairedDevices.size() > 0) {
+                for (BluetoothDevice device : pairedDevices) {
+                    mPairedDevicesArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+                }
+            }
+        }
+        //---------------------------------------------------------------------
+    }
+
+    // Configura un (on-click) para la lista
+    private AdapterView.OnItemClickListener mDeviceClickListener = new AdapterView.OnItemClickListener() {
+        public void onItemClick(AdapterView av, View v, int arg2, long arg3) {
+
+            // Obtener la dirección MAC del dispositivo
+            String info = ((TextView) v).getText().toString();
+            String address = info.substring(info.length() - 17);
+            finishAffinity();
+
+            // Realiza un intent para iniciar la siguiente actividad
+            Intent intend = new Intent(DispositifConnecter.this, MainActivity.class);
+            intend.putExtra(EXTRA_DEVICE_ADDRESS, address);
+            startActivity(intend);
+        }
+    };
+
+    private void VerificarEstadoBT() {
+        // Comprueba que el dispositivo tiene Bluetooth y que está encendido.
+        mBtAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBtAdapter == null) {
+            Toast.makeText(getBaseContext(), "El dispositivo no soporta Bluetooth", Toast.LENGTH_SHORT).show();
+        } else {
+            if (mBtAdapter.isEnabled()) {
+                Log.d(TAG, "...Bluetooth Activado...");
+            } else {
+                //Solicita al usuario que active Bluetooth
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                    //return;
+                    startActivityForResult(enableBtIntent, 1);
+                }
+            }
+        }
     }
 }
--- Learnobot/app/src/main/java/com/example/learnobot/MainActivity.java
+++ Learnobot/app/src/main/java/com/example/learnobot/MainActivity.java
@@ -1,21 +1,270 @@
 package com.example.learnobot;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.UUID;
 
 public class  MainActivity extends AppCompatActivity {
 
     private Button mBas, mHaut, mDroite, mGauche, mOk, mDeconnexion;
+
+    //-------------------------------------------
+    Handler bluetoothIn;
+    final int handlerState = 0;
+    private BluetoothAdapter btAdapter = null;
+    private BluetoothSocket btSocket = null;
+    private StringBuilder DataStringIN = new StringBuilder();
+    private ConnectedThread MyConexionBT;
+    // Identificador unico de servicio - SPP UUID
+    private static final UUID BTMODULEUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+    // String para la direccion MAC
+    private static String address = null;
+    //-------------------------------------------
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        bluetoothIn = new Handler() {
+            public void handleMessage(android.os.Message msg) {
+                if (msg.what == handlerState) {
+                    //Interacción con los datos de ingreso
+              /*      char MyCaracter = (char) msg.obj;
+
+                    if (MyCaracter == 'a') {
+                        tvtMensaje.setText("ACELERANDO");
+                    }
+
+                    if (MyCaracter == 'i') {
+                        tvtMensaje.setText("GIRO IZQUIERDA");
+                    }
+
+                    if (MyCaracter == 'd') {
+                        tvtMensaje.setText("GIRO DERECHA");
+                    }
+
+                    if (MyCaracter == 'r') {
+                        tvtMensaje.setText("RETROCEDIENDO");
+                    }
+
+                    if (MyCaracter == 's') {
+                        tvtMensaje.setText("DETENIDO");
+                    }*/
+                }
+            }
+        };
+
+        btAdapter = BluetoothAdapter.getDefaultAdapter();
+        VerificarEstadoBT();
+        //-----------------------------------------------------------------------
+
+
+
+        mBas = findViewById(R.id.idBas);
+        mHaut = findViewById(R.id.idHaut);
+        mDroite = findViewById(R.id.idDroite);
+        mGauche = findViewById(R.id.idGauche);
+        mDeconnexion = findViewById(R.id.idDeconnexion);
+
+        mBas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MyConexionBT.write("B");
+            }
+        });
+        mHaut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MyConexionBT.write("H");
+            }
+        });
+        mDroite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MyConexionBT.write("D");
+            }
+        });
+        mGauche.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MyConexionBT.write("G");
+            }
+        });
+
+        mDeconnexion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (btSocket != null) {
+                    try {
+                        btSocket.close();
+                    } catch (IOException e) {
+                        Toast.makeText(getBaseContext(), "Error", Toast.LENGTH_SHORT).show();
+                        ;
+                    }
+                }
+                finish();
+            }
+        });
+
+    }
+
+
+    //xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+    private BluetoothSocket createBluetoothSocket(BluetoothDevice device) throws IOException {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+        }
+        return device.createRfcommSocketToServiceRecord(BTMODULEUUID);
+        //creates secure outgoing connecetion with BT device using UUID
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        Intent intent = getIntent();
+        address = intent.getStringExtra(DispositifConnecter.EXTRA_DEVICE_ADDRESS);
+        //Setea la direccion MAC
+        BluetoothDevice device = btAdapter.getRemoteDevice(address);
+
+        try {
+            btSocket = createBluetoothSocket(device);
+        } catch (IOException e) {
+            Toast.makeText(getBaseContext(), "La creacción del Socket fallo", Toast.LENGTH_LONG).show();
+        }
+        // Establece la conexión con el socket Bluetooth.
+        try {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                btSocket.connect();
+                //Toast.makeText(getBaseContext(), "CONEXION EXITOSA", Toast.LENGTH_SHORT).show();
+
+                //return;
+            }
+
+            //btSocket.connect();
+        } catch (IOException e) {
+            try {
+                btSocket.close();
+            } catch (IOException e2) {
+            }
+        }
+        MyConexionBT = new ConnectedThread(btSocket);
+        MyConexionBT.start();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        try { // Cuando se sale de la aplicación esta parte permite que no se deje abierto el socket
+            btSocket.close();
+        } catch (IOException e2) {
+        }
+    }
+
+    //Comprueba que el dispositivo Bluetooth
+    //está disponible y solicita que se active si está desactivado
+    private void VerificarEstadoBT() {
+
+        if (btAdapter == null) {
+            Toast.makeText(getBaseContext(), "El dispositivo no soporta bluetooth", Toast.LENGTH_LONG).show();
+        } else {
+            if (btAdapter.isEnabled()) {
+            } else {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    startActivityForResult(enableBtIntent, 1);
+                    //return;
+                }
+
+            }
+        }
+    }
+
+    //Crea la clase que permite crear el evento de conexion
+    private class ConnectedThread extends Thread
+    {
+        private final InputStream mmInStream;
+        private final OutputStream mmOutStream;
+
+        public ConnectedThread(BluetoothSocket socket)
+        {
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
+            try
+            {
+                tmpIn = socket.getInputStream();
+                tmpOut = socket.getOutputStream();
+            } catch (IOException e) { }
+            mmInStream = tmpIn;
+            mmOutStream = tmpOut;
+        }
+
+        public void run()
+        {
+            byte[] byte_in = new byte[1];
+            // Se mantiene en modo escucha para determinar el ingreso de datos
+            while (true) {
+                try {
+                    mmInStream.read(byte_in);
+                    char ch = (char) byte_in[0];
+                    bluetoothIn.obtainMessage(handlerState, ch).sendToTarget();
+                } catch (IOException e) {
+                    break;
+                }
+            }
+        }
+
+        //Envio de trama
+        public void write(String input)
+        {
+            try {
+                mmOutStream.write(input.getBytes());
+            }
+            catch (IOException e)
+            {
+                //si no es posible enviar datos se cierra la conexión
+                Toast.makeText(getBaseContext(), "La Conexión fallo", Toast.LENGTH_LONG).show();
+                finish();
+            }
+        }
+    }
+    //xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+}
+
+
+/*----------------------------------------------------------------------
         mBas = findViewById(R.id.idBas);
         mHaut = findViewById(R.id.idHaut);
         mDroite = findViewById(R.id.idDroite);
@@ -54,4 +303,4 @@ public class  MainActivity extends AppCompatActivity {
             }
         });
     }
-}
+*/
