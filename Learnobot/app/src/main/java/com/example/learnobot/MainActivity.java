c7491bfb0186d7bb21f2b1d5e223373d9964c3e4
Odiouma <odioumacoulibaly46@gmail.com>
2022-12-19 16:08:24 +0100

    Ecoute des boutons

--- Learnobot/app/src/main/java/com/example/learnobot/MainActivity.java
+++ Learnobot/app/src/main/java/com/example/learnobot/MainActivity.java
@@ -2,13 +2,61 @@ package com.example.learnobot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class  MainActivity extends AppCompatActivity {
 
+    private Button mBas, mHaut, mDroite, mGauche, mOk, mDeconnexion;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBas = findViewById(R.id.idBas);
+        mHaut = findViewById(R.id.idHaut);
+        mDroite = findViewById(R.id.idDroite);
+        mGauche = findViewById(R.id.idGauche);
+        mDeconnexion = findViewById(R.id.idDeconnexion);
+
+        mBas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        mHaut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        mDroite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        mGauche.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        mOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        mDeconnexion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
